cb65a63e3fbd8db4f69e27e00ea36baec86f5562
Erikas_Svazas <erikas.svazas@ff.stud.vu.lt>
2021-04-06 00:23:13 +0300

    Add LSTM functionality: testing needed.

--- solver/processing.py
+++ solver/processing.py
@@ -45,6 +45,40 @@ def smooth(df, cols_to_smooth, ma_constant=3):
     return df.dropna()
 
 
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    """
+    Frame a time series as a supervised learning dataset.
+    Arguments:
+        data: Sequence of observations as a list or NumPy array.
+        n_in: Number of lag observations as input (X).
+        n_out: Number of observations as output (y).
+        dropnan: Boolean whether or not to drop rows with NaN values.
+    Returns:
+        Pandas DataFrame of series framed for supervised learning.
+    """
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = pd.DataFrame(data)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+    # put it all together
+    agg = pd.concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
+
+
 def plot_predictions(y_true, y_predict, N_OUT=1, plot_samples=800, save=False):
     ''' Plot predictions and true values. '''
     fig = plt.figure(figsize=(14, 5))
