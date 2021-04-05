c37fee06a71ee6ad486fe5166f4725b3cb738886
Erikas_Svazas <erikas.svazas@ff.stud.vu.lt>
2021-04-05 23:37:31 +0300

    Add smooth function.

--- solver/processing.py
+++ solver/processing.py
@@ -2,20 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-import tensorflow as tf
-from tensorflow import keras
-
-
-from sklearn.metrics import mean_squared_error, mean_absolute_error
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
-from keras.optimizers import Adam, SGD
-from keras.wrappers.scikit_learn import KerasRegressor
-from keras.callbacks import EarlyStopping
-
-
 def smape(a, b):
     '''
     Calculates sMAPE
@@ -35,8 +21,16 @@ def smape(a, b):
     return sm
 
 
+def read_dataset(data_link=None):
+    ''' Read dataset from a passed link. '''
+    if data_link is not None:
+        data = pd.read_csv(data_link, encoding='utf-8', sep='\t')
+        data['Date_time'] = pd.to_datetime(data['Date_time'], utc=True, errors='coerce')
+        return data
+
+
 def read_datasets(turbine_link=None, weather_link=None):
-    ''' Read passed links of dataframes. '''
+    ''' Read passed links of DataFrames. '''
     if weather_link is not None:
         weather_data = pd.read_csv(weather_link, encoding='utf-8')
         weather_data['Date_time'] = pd.to_datetime(weather_data['date_time'], utc=True, errors='coerce')
@@ -46,50 +40,9 @@ def read_datasets(turbine_link=None, weather_link=None):
     return weather_data, turbine_data
 
 
-def fit_lstm(X, y, X_val, y_val, batch_size, nb_epochs, neurons, n_past):
-    '''
-    Fit LSTM model on the X, y data with the defined paramters
-    :param X: training inputs (2D array)
-    :param y: training outputs (1D array)
-    :param X_val: validation inputs (2D array)
-    :param y_val: validation outputs (1D array)
-    :param batch_size: batch size for the LSTM training
-    :param nb_epochs: number of epochs to train
-    :param neurons: number of neurons in LSTM layers
-    :param n_past: number of past samples for every observation
-    :return: LSTM model
-    '''
-    # shape input data
-    X = X.reshape(-1, n_past, int(X.shape[1] / n_past))
-    X_val = X_val.reshape(-1, n_past, int(X_val.shape[1] / n_past))
-
-    # define model
-    model = Sequential()
-    model.add(LSTM(neurons, activation='relu', input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
-    model.add(LSTM(neurons, activation='relu', input_shape=(X.shape[1], X.shape[2])))
-    model.add(Dense(1))
-    model.compile(loss='mse', optimizer='adam')
-    # fit model
-    model.fit(
-        X, y, epochs=nb_epochs, batch_size=batch_size,
-        verbose=1, validation_data=(X_val, y_val))
-    return model
-
-
-def make_predictions(model, X, y, scaler, n_past):
-    # shape input data
-    X = X.reshape(-1, n_past, int(X.shape[1]/n_past))
-    # make predictions
-    y_predict = model.predict(X)
-    # scale outputs back
-    y_true = scaler.inverse_transform(y.reshape(-1,1)).flatten()
-    y_predict = scaler.inverse_transform(y_predict.reshape(-1,1)).flatten()
-    # evaluate predictions
-    print("MAE: {} RMSE: {} sMAPE: {}".format(
-        round(mean_absolute_error(y_true, y_predict), 3),
-        round(np.sqrt(mean_squared_error(y_true, y_predict)),3),
-        round(smape(y_true, y_predict),3)))
-    return y_predict, y_true
+def smooth(df, cols_to_smooth, ma_constant=3):
+    df[cols_to_smooth] = df[cols_to_smooth].rolling(ma_constant).mean()
+    return df.dropna()
 
 
 def plot_predictions(y_true, y_predict, N_OUT=1, plot_samples=800, save=False):
